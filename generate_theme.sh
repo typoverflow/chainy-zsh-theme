d8c8acef01ed28f378be0071183002f4268675d1
typoverflow <typoverflow@outlook.com>
2024-08-24 14:14:53 +0800

    update: git info

--- generate_theme.sh
+++ generate_theme.sh
@@ -138,6 +138,15 @@ function conda_prompt_info() {
 	fi
 }
 
+# get the name of the branch we are on
+function git_prompt_info() {
+  if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
+    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  fi
+}
+
 # get ip
 function ip_prompt_info() {
 	local ip="$(ip a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '\''{print $2}'\'' | awk -F "/" '\''{print $1}'\'' | head -n 1)"
